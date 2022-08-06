cfbf0870463f5c167e45d41f27da50945003f5d8
unknown <pablorodrz09@gmail.com>
2022-08-06 19:42:56 -0300

    final project

---
+++ src/components/Advice.jsx
@@ -0,0 +1,39 @@
+import { React, useState, useEffect } from 'react'
+import dice from '../images/icon-dice.svg'
+import underline from '../images/pattern-divider-desktop.svg'
+import mobileUnderline from '../images/pattern-divider-mobile.svg'
+const Advice = () => {
+  const [isLoading, setIsLoading] = useState(true)
+  const [advices, setAdvices] = useState({})
+  console.log("render")
+  const newAdvice = () => {
+    setIsLoading(true)
+    fetch("https://api.adviceslip.com/advice?t=" + Math.random())
+      .then(response => response.json())
+      .then(data => { setAdvices(advice => advice = data); setIsLoading(false) })
+  }
+
+  useEffect(() => {
+    newAdvice()
+  }, [])
+
+  return (
+    <div className="advice-container">
+      <div className='advices'>
+      {isLoading && <div className='loading'>
+        <h1>Loading...</h1>
+      </div>}
+      {!isLoading && <div className='advice'>
+        <h4>{`ADVICE #${advices?.slip?.id}`} </h4>
+        <h3>{advices?.slip?.advice}</h3>
+        <img className='underline' src={window.innerWidth <= 412 ? mobileUnderline : underline} alt="underline" />
+        <button className='btn' onClick={newAdvice}> <img src={dice} alt="dice" className='btn-diceimg'/> </button>
+      </div>}
+      </div>
+    </div>
+
+
+  )
+}
+
+export default Advice
